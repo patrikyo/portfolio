cf707febd83f93d3dfa08431b0da4f2b62cf5575
youpa <patrik.youssef@arbetsformedlingen.se>
2025-03-24 15:48:12 +0100

    Adds animation work for toggle

--- src/app/components/HamburgerMenu/HamburgerMenu.tsx
+++ src/app/components/HamburgerMenu/HamburgerMenu.tsx
@@ -2,13 +2,39 @@
 import HamburgerMenuProps from "@/app/models/interfaces/hamburgerMenuProps.interface";
 import styles from "./HamburgerMenu.module.css";
 import { useState } from "react";
+import { faBars, faClose } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const HamburgerMenu: React.FC<HamburgerMenuProps> = ({ links }) => {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
   return (
-    <nav className={styles.nav}>
-      {isMenuOpen && (
-        <ul className={styles.listContainer}>
+    <div className={styles.container}>
+      {/* TOGGLE btn */}
+      <button
+        className={styles.toggleBtn}
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      >
+        <span
+          className={`${styles.icon} ${!isMenuOpen ? styles.iconHidden : ""}`}
+        >
+          <FontAwesomeIcon icon={faClose} size="3x" color="#ffff" />
+        </span>
+
+        <span
+          className={`${styles.icon} ${isMenuOpen ? styles.iconHidden : ""}`}
+        >
+          <FontAwesomeIcon icon={faBars} size="3x" color="#ffff" />
+        </span>
+      </button>
+
+      {/* NAV list*/}
+      <nav>
+        <ul
+          className={`${styles.listContainer} ${
+            isMenuOpen ? styles.textVisible : ""
+          }`}
+        >
           {links.map((ele, i) => {
             return (
               <li key={i}>
@@ -19,8 +45,8 @@ const HamburgerMenu: React.FC<HamburgerMenuProps> = ({ links }) => {
             );
           })}
         </ul>
-      )}
-    </nav>
+      </nav>
+    </div>
   );
 };
 
