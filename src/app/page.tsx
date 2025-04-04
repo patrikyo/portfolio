1d819d02b410b795223201e9f24ccc44a7df6513
youpa <patrik.youssef@arbetsformedlingen.se>
2025-04-04 18:08:52 +0200

    Adds hero and about  to contianer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./components/Header/Header";
+import HeroSection from "./components/HeroSection/HeroSection";
+import About from "./components/About/About";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Header />
+      <HeroSection />
+      <About />
     </div>
   );
 }
